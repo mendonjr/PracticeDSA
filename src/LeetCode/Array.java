d8c09d1a11b36da2bb99e1a9eb15808f554f80f2
mendonjr <prasadmendon744@gmail.com>
2022-07-18 14:32:46 +0530

    Revert "saturday array practice"
    This reverts commit 6952a28aac056e474ed367a96a20cb2b749dd0b4.

--- src/LeetCode/Array.java
+++ src/LeetCode/Array.java
@@ -5,16 +5,14 @@ import java.util.List;
 public class Array {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        int[] nums = {1,1,1,1};
+        int[] nums = {2, 3, 5, 1, 3};
         // int[][] accounts = {{1, 2, 3}, {3, 2, 1}};
 
         // int[] ans = buildArray(nums);
         //int ans = maximumWealth(accounts);
         // int[] ans = shuffle(nums, 2);
         List<Boolean> ans = kidsWithCandies(nums, 3);
-
-        int answ = numIdenticalPairs(nums);
-        System.out.println(answ);
+        System.out.println(ans);
     }
 
     static int[] buildArray(int[] nums) {
@@ -71,26 +69,12 @@ public class Array {
             for (int candy : candies) {
                 if (temp < candy) {
                     isGreater = false;
-                    break;
                 }
             }
             result.add(isGreater);
         }
         return result;
 
-
-    }
-
-    static int numIdenticalPairs(int[] nums) {
-        int count = 0;
-        for(int i=0;i<=nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-        }
-        return count;
 
     }
 
